ac8bca56ede02e80dc23e5f986f3192d6bacd939
hbb_mac <kok.bing@qq.com>
2025-07-06 17:07:01 +0800

    perf - optimizing Binding Performance

--- include/prism/rn/prismmodelproxy.hpp
+++ include/prism/rn/prismmodelproxy.hpp
@@ -2,16 +2,21 @@
 #define PRISM_RN_CORE_PRISM_MODEL_PROXY_HPP
 
 #include "ReactCommon/CallInvoker.h"
-#include "prism/rn/prismLog.h"
+#include "include/prism/rn/prismLog.h"
 #include "prism/rn/prismmodellistproxy.hpp"
 #include "prism/utilities/typeName.hpp"
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
 #include <jsi/jsi.h>
+#include <map>
 #include <memory>
 #include <ostream>
 #include <prism/container.hpp>
 #include <prism/prism.hpp>
 #include <string>
+#include <tuple>
+#include <type_traits>
 
 namespace prism
 {
@@ -88,9 +93,7 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
         std::string propName = name.utf8(rt);
         bool hasField = false;
 
-        //#ifdef DEBUG
-        //        auto jsinvoke = prism::Container::get()->resolve_object<facebook::react::CallInvoker>();
-        //#endif
+        // auto jsinvoke = prism::Container::get()->resolve_object<facebook::react::CallInvoker>();
 
         facebook::jsi::Value result;
         using namespace prism::reflection;
@@ -132,10 +135,59 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
                  });
         if (!hasField)
         {
+
             if (propName == "uuid")
             {
                 result = facebook::jsi::String::createFromUtf8(rt, std::to_string(reinterpret_cast<long long>(this)));
                 return result;
+            }
+            else if (propName == "register")
+            {
+                return jsi::Function::createFromHostFunction(rt,   // runtime
+                                                             name, //函数名
+                                                             2,    // 参数个数
+                                                             [this](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value
+                                                             {
+                                                                 if (count >= 2 && args[0].isString() && args[1].isObject())
+                                                                 {
+                                                                     auto fname = args[0].toString(rt);
+                                                                     std::shared_ptr<jsi::Function> func = std::make_shared<jsi::Function>(args[1].asObject(rt).asFunction(rt));
+                                                                     if (func)
+                                                                     {
+                                                                         size_t int_key = reinterpret_cast<size_t>(func.get());
+                                                                         std::string key = std::to_string(int_key);
+                                                                         notifyui_[key] = std::make_tuple(fname.utf8(rt), func);
+                                                                         // LOG_INFO_F(rt, jsinvoke, "after registe map size:{}", notifyui_.size());
+                                                                         return jsi::String::createFromUtf8(rt, key);
+                                                                     }
+                                                                     return jsi::String::createFromUtf8(rt, "");
+                                                                 }
+                                                                 else
+                                                                 {
+                                                                     return jsi::String::createFromUtf8(rt, "");
+                                                                 }
+                                                             });
+            }
+            else if (propName == "unregister")
+            {
+                return jsi::Function::createFromHostFunction(rt,   // runtime
+                                                             name, //函数名
+                                                             1,    // 参数个数
+                                                             [this](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value
+                                                             {
+                                                                 if (count >= 1 && args[0].isString())
+                                                                 {
+                                                                     auto key = args[0].toString(rt).utf8(rt);
+                                                                     notifyui_.erase(key);
+                                                                     // LOG_INFO_F(rt, jsinvoke, "after unregiste map size:{}", notifyui_.size());
+                                                                     return args[0].toString(rt);
+                                                                 }
+                                                                 else
+                                                                 {
+                                                                     // not found return undefine
+                                                                     return jsi::String::createFromUtf8(rt, "");
+                                                                 }
+                                                             });
             }
             return facebook::jsi::Value::undefined();
         }
@@ -170,7 +222,11 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
                                                           prism::utilities::is_specialization<FieldType, std::unique_ptr>::value ||
                                                           prism::utilities::is_specialization<FieldType, std::weak_ptr>::value || std::is_pointer_v<FieldType>)
                                             {
-                                                field = nullptr;
+                                                if (field != nullptr)
+                                                {
+                                                    field = nullptr;
+                                                    this->notifyUi(&rt, name);
+                                                }
                                             }
                                         });
         }
@@ -184,21 +240,35 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
                                             using FieldType = std::remove_reference_t<std::remove_reference_t<decltype(field)>>;
                                             if constexpr (std::is_same_v<FieldType, bool>)
                                             {
-                                                field = boolValue;
+                                                if (field != boolValue)
+                                                {
+                                                    field = boolValue;
+                                                    this->notifyUi(&rt, name);
+                                                }
                                             }
                                         });
         }
         else if (value.isNumber())
         {
+            auto invoker = prism::Container::get()->resolve_object<facebook::react::CallInvoker>();
+
+            LOG_INFO_F(rt, invoker, "111111");
             // Use numberValue
             double numberValue = value.getNumber();
             prism::reflection::field_do(prismobj, propName.c_str(),
                                         [&](auto &&field)
                                         {
+                                            LOG_INFO_F(rt, invoker, "111112");
                                             using FieldType = std::remove_reference_t<std::remove_reference_t<decltype(field)>>;
                                             if constexpr (std::is_same_v<FieldType, double> || std::is_same_v<FieldType, float> || std::is_same_v<FieldType, int>)
                                             {
-                                                field = numberValue;
+                                                LOG_INFO_F(rt, invoker, "111113,left:{},right{}", field, numberValue);
+                                                if (std::abs(field - numberValue) >= 0.000001)
+                                                {
+                                                    LOG_INFO_F(rt, invoker, "111114");
+                                                    field = numberValue;
+                                                    this->notifyUi(&rt, name);
+                                                }
                                             }
                                         });
         }
@@ -212,7 +282,11 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
                                             using FieldType = std::remove_reference_t<std::remove_reference_t<decltype(field)>>;
                                             if constexpr (std::is_same_v<FieldType, std::string>)
                                             {
-                                                field = stringValue;
+                                                if (field != stringValue)
+                                                {
+                                                    field = stringValue;
+                                                    this->notifyUi(&rt, name);
+                                                }
                                             }
                                         });
         }
@@ -232,11 +306,9 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
     // Override setProperty to handle property assignments from JavaScript
     void set(facebook::jsi::Runtime &rt, const facebook::jsi::PropNameID &name, const facebook::jsi::Value &value) override
     {
+
         std::string propName = name.utf8(rt);
-        if (propName == "notifyUI")
-            this->notifyui_ = std::make_shared<jsi::Function>(value.asObject(rt).asFunction(rt));
-        else
-            remove_pointer_set(*this->instance_, rt, propName, value);
+        remove_pointer_set(*this->instance_, rt, propName, value);
     }
 
     std::vector<facebook::jsi::PropNameID> getPropertyNames(facebook::jsi::Runtime &rt) override
@@ -249,16 +321,48 @@ template <typename T> class PrismModelProxy : public facebook::jsi::HostObject
 
     void notifyUi(jsi::Runtime *rt)
     {
-        if (!notifyui_)
+        if (!notifyui_.size())
             return;
         if (!rt)
             return;
-        auto r = ::facebook::jsi::Object::createFromHostObject(*rt, std::make_shared<prism::rn::PrismModelProxy<T>>(this->instance_));
-        notifyui_->call(*rt, r);
+        // auto r = ::facebook::jsi::Object::createFromHostObject(*rt, std::make_shared<prism::rn::PrismModelProxy<T>>(this->instance_));
+        for (auto [k, v] : notifyui_)
+        {
+            // k : pointer adderess str
+            // v : tuple<fname,js_function>
+            std::string fname = std::get<0>(v);
+            prism::reflection::field_do(*this->instance_, fname.c_str(), [&](auto &&value) { std::get<1>(v)->call(*rt, value); });
+        }
+    }
+    void notifyUi(jsi::Runtime *rt, std::string field_name)
+    {
+        if (!notifyui_.size())
+            return;
+        if (!rt)
+            return;
+        // auto r = ::facebook::jsi::Object::createFromHostObject(*rt, std::make_shared<prism::rn::PrismModelProxy<T>>(this->instance_));
+        for (auto [k, v] : notifyui_)
+        {
+            // k : pointer adderess str
+            // v : tuple<fname,js_function>
+            std::string fname = std::get<0>(v);
+            if (field_name == fname)
+                prism::reflection::field_do(*this->instance_, fname.c_str(),
+                                            [&](auto &&value)
+                                            {
+                                                using FieldType = std::remove_reference_t<std::remove_reference_t<decltype(value)>>;
+
+                                                if constexpr (!(prism::utilities::is_specialization<FieldType, std::shared_ptr>::value ||
+                                                                prism::utilities::is_specialization<FieldType, std::unique_ptr>::value ||
+                                                                prism::utilities::is_specialization<FieldType, std::weak_ptr>::value || std::is_pointer_v<FieldType>)&&!std::is_class_v<FieldType>)
+
+                                                    std::get<1>(v)->call(*rt, value);
+                                            });
+        }
     }
 
   private:
-    std::shared_ptr<jsi::Function> notifyui_ = nullptr;
+    std::map<std::string, std::tuple<std::string, std::shared_ptr<jsi::Function>>> notifyui_;
     std::shared_ptr<T> instance_;
 };
 
